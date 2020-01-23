70a412d46f08ad417d06f3a8b0fcfa8c078758a1
g-w1 <jacoblevgw@gmail.com>
2020-01-23 16:19:49 -0500

    update

--- autoencode.py
+++ autoencode.py
@@ -8,7 +8,7 @@ f.close()
 training_data = [(x[0],x[0]) for x in training_data]
 learningrate = 3
 mini_batch_size = 2
-epochs = random.randint(5,11)
+epochs = 50
 net = Network(layers)
 net.SGD(training_data, epochs, mini_batch_size, learningrate)
 f = open("data/data_2.data","wb")
--- docs/sketch.js
+++ docs/sketch.js
@@ -1,57 +1,43 @@
 
-async function getData(inputs,sliders){
+async function getData(inputs){
+    //console.log(inputs);
     const response = await fetch("https://g-w1.github.io/first-AE/weights_biases.json");
     const mydata = await response.json();
     var weights = mydata.weights;
     var biases = mydata.biases;
-    var inputs = [];
-    for (var i = 0; i<length(sliders);i++){
-        inputs.push(sliders);
-    }
+    inputs = math.reshape(inputs,[10,1])
+    inputs = math.multiply(inputs,1000);
+    // console.log(weights[1]);
     output = sigmoid(math.add(math.multiply(weights[1],inputs),biases[1]));
-    output = math.reshpe(output,[625,625]);
+    output = math.reshape(output,[25,25]);
     for (var i = 0; i<25; i++){
         for (var h = 0; h<25; h++){
-            rect(h,i,20,20);
+            fill(output[i][h]*100);
+            rect(h*20,i*20,20,20);
         }
+    // console.log(output);
     }
     }
 
 
-
+    var sliders = [];  
   function setup() {
-    var sliders = [];
+    
     for (var i = 0; i<10; i++){
         sliders.push(createSlider(0,100,50));
     }
-    var canv = createCanvas(500, 500);
+    let canv = createCanvas(500, 500);
     canv.parent("canvasbox");
     background(0);
-    let guessButton = select('#guess');
-    guessButton.mousePressed(function() {
-      let inputs = []
-      let img = get();
-      img.resize(25, 25);
-      img.loadPixels();
-      for (let i = 0; i <625; i++) {
-        let bright = img.pixels[i * 4];
-        inputs.push([bright/255.0]);
-  
-      }
-  console.log(predict(inputs))
-  })
-    let clearButton = select("#clear");
-    clearButton.mousePressed(function(){
-        background(0);
-        document.getElementById("header").innerHTML = "Draw a Smiley face or a Frowney face and hit guess to make the computer classify it";
-    })
-  }
-  function draw() {
-    getData(inputs,sliders);
-  }
-  function predict(inputs){
     
-        
+  }
+  
+  function draw() {
+    inputs = [];
+    for (let i = 0;i<10;i++){
+      inputs.push(sliders[i].value());
+    }
+    getData(inputs);
   }
   function sigmoid(z){
   
