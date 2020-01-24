c08476c3fb2693bab1023b3452bd06852376e240
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-24 17:56:27 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -34,7 +34,7 @@ async function getData(inputs){
   
   function draw() {
     inputs = [];
-    for (let i = 0;i<10;i++){
+    for (let i = 0;i<20;i++){
       inputs.push(sliders[i].value());
     }
     getData(inputs);
